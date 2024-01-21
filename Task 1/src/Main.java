252652962de337c394b07a7c2aa1349d9a6bf19d
ДимаПрибытков <d.pribytkov93@mail.ru>
2024-01-21 13:05:09 +0300

    рефакторинг кода

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,18 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+
     Phone nokia = new Phone(" 7777", "nokia", 12.5);
+        nokia.receiveCall("Иван ");
         System.out.println("Номер телефона: " + nokia.number + " Модель телефона: " + nokia.model +
                 " Вес телефона: " + nokia.weight);
     nokia.manufacturerСountry = "Japan";
     nokia.country();
-    nokia.receiveCall("Иван ", "8890");
 
 
     Phone samsung = new Phone(" 9999", "samsung", 55.2);
+        samsung.receiveCall("Саша ");
     System.out.println("Номер телефона: " + samsung.number + " Модель телефона: " + samsung.model +
                 " Вес телефона: " + samsung.weight);
-        nokia.manufacturerСountry = "Japan";
-        samsung.receiveCall("Саша ", "4444");
+        samsung.manufacturerСountry = "Japan";
+        samsung.country();
+
 
     }
 }
